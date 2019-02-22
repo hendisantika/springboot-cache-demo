6850dfdb545e4637a42d61cd1ee9455c253514e6
Hendi Santika <hendi.santika@allianz.co.id>
2019-02-22 15:08:51 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootcachedemo/SpringbootCacheDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootcachedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCacheDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCacheDemoApplication.class, args);
+    }
+
+}
